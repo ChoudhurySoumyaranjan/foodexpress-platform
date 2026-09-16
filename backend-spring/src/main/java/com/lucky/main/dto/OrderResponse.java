360d4b99e8db01e173b8785da531c170a3fb1fdf
soumya <choudhurysoumya87@gmail.com>
2026-09-17 00:39:15 +0530

    Redis implemented in Order Module

--- backend-spring/src/main/java/com/lucky/main/dto/OrderResponse.java
+++ backend-spring/src/main/java/com/lucky/main/dto/OrderResponse.java
@@ -6,14 +6,18 @@ import com.lucky.main.enums.PaymentStatus;
 import com.lucky.main.enums.PaymentType;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderResponse {
 
     private Long orderId;
